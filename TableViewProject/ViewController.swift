623b1ef96e26a0d7701a14d0f580a1fd83e3eef1
serkanboyraci <serkanboyraci@gmail.com>
2022-11-28 16:03:37 +0300

    we learn protocols from UITableView.

--- TableViewProject/ViewController.swift
+++ TableViewProject/ViewController.swift
@@ -7,12 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //tableView.dataSource = self
+        //tableView.delegate = self
+        
     }
+    
+    // var view = UIViewController
+    // when you look at UIViewController, it s just class. we inherited from it.
+    // we can add UITableViewDelegate and DataSource which are Protocol. If we add them, we have to add 2 non-optional functions
+    
+    // we need these two func to create cell and change cell.
+    // just because of this, we use protocols. we can define protocols for our own apps.
+    
+    
+    
+    
 
 
 }
